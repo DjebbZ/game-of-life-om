0da6fb2db142c8a65b0af5bbbb8eadcb1bf400ba
DjebbZ <khalid.jebbari@gmail.com>
2014-03-19 23:01:09 +0100

    Log component lifecycle

--- src/game_of_life_om/core.cljs
+++ src/game_of_life_om/core.cljs
@@ -6,8 +6,41 @@
 
 (def app-state (atom {:text "Hello world!"}))
 
+(defn my-widget [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+                (prn "IIniState")
+                nil)
+    om/IRender
+    (render [_]
+            (prn "IRender")
+            (dom/h1 nil (:text data)))
+    om/IWillMount
+    (will-mount [_]
+                (prn "IWillUnmount"))
+    om/IDidMount
+    (did-mount [_]
+               (prn "IDidMount"))
+    om/IWillUpdate
+    (will-update [_ _ _]
+                 (prn "IWillUpdate"))
+    om/IDidUpdate
+    (did-update [_ _ _]
+                (prn "IDidUpdate"))
+    om/IWillUnmount
+    (will-unmount [_]
+                     (prn "IWillUnmount"))
+    om/IShouldUpdate
+    (should-update [_ _ _]
+                   (prn "IShouldUpdate")
+                   true)))
+
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+  my-widget
   app-state
   {:target (. js/document (getElementById "app"))})
+
+(swap! app-state (fn [state]
+                   (assoc state :text "Test swa5")))
+
